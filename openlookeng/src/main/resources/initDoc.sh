4deab1cde1e4074e2c24b34084d569ffe4503ae5
陈阳 <775517776@qq.com>
2023-11-20 11:05:51 +0800

    first

--- openlookeng/src/main/resources/initDoc.sh
+++ openlookeng/src/main/resources/initDoc.sh
@@ -5,7 +5,7 @@ TARGET=/docs-file/target
 mkdir -p ${SOURCE}
 # shellcheck disable=SC2164
 cd ${SOURCE}
-git clone https://${GITEE_USER}:${GITEE_PASSWORD}gitee.com/openlookeng/website-v2.git
+git clone https://gitee.com/openlookeng/website-v2.git
 git clone https://gitee.com/openlookeng/website-docs.git
 
 mkdir -p ${SOURCE}
